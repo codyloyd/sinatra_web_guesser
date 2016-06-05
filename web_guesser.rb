9195585e443df30607fa4a755567d3b182db44eb
Cody Loyd <codyloyd@gmail.com>
2016-06-05 18:28:31 -0500

    refactor

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,12 +1,32 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret_number = rand 100
+Secret_number = rand 100
 
 get '/' do
-  guess = params['guess'].to_i
-  eval = guess <=> secret_number
-  diff = guess - secret_number
-  erb :index, :locals => {:number => secret_number, :guess => guess, :eval => eval, :diff => diff}
+  guess = params["guess"].to_i
+  message = check_guess(guess)
+  erb :index, :locals => {:number => Secret_number, :guess => guess, :message => message}
 end
 
+def check_guess(guess)
+  message = ""
+  eval = guess <=> Secret_number
+  diff = guess - Secret_number 
+
+  if diff.abs >= 5
+    message += "<em>a lot</em>"
+  elsif diff == 0
+  end
+
+  case eval
+  when 1
+    message += " bigger than"
+  when 0 
+    message += " the same as"
+  when -1
+    message += " smaller than"
+  end
+  
+  message
+end
