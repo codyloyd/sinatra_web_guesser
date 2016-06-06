3ac06442921f532578c0da12a8b70c4650dbe1e1
Cody Loyd <codyloyd@gmail.com>
2016-06-05 23:58:50 -0500

    added a bunch of stuff

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,21 +1,43 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-Secret_number = rand 100
+@@secret_number = rand 100
+@@guesses_remaining = 5
 
 get '/' do
   guess = params["guess"].to_i
-  message = check_guess(guess)
-  erb :index, :locals => {:number => Secret_number, :guess => guess, :message => message}
+  guess_response = check_guess(guess)
+  guesses
+  erb :index, :locals => {:number => @@secret_number, 
+                          :guess => guess, 
+                          :message => guess_response[0], 
+                          :color => guess_response[1],
+                          :guesses_remaining => @@guesses_remaining }
+end
+
+get '/loser' do
+  erb :loser, :locals => {:number => @@secret_number}
+end
+
+def guesses
+  if @@guesses_remaining > 0
+    @@guesses_remaining -= 1
+  else
+    @@secret_number = rand 100
+    @@guesses_remaining = 5
+    redirect "/loser"
+  end
 end
 
 def check_guess(guess)
   message = ""
+  color = "yellow"
   eval = guess <=> Secret_number
   diff = guess - Secret_number 
 
   if diff.abs >= 5
     message += "<em>a lot</em>"
+    color = "red"
   elsif diff == 0
   end
 
@@ -24,9 +46,10 @@ def check_guess(guess)
     message += " bigger than"
   when 0 
     message += " the same as"
+    color = "blue"
   when -1
     message += " smaller than"
   end
   
-  message
+  [message,color]
 end
